016b5e397652bf94ffb11d21a3e9da45420d5fae
Spidust <tranhaidangg2@gmail.com>
2024-02-17 10:03:03 +0700

    login and register handle

--- src/API/User.js
+++ src/API/User.js
@@ -9,6 +9,37 @@ class UserAPI {
     }
     return data.result;
   }
+
+  async login(username, password) {
+    const res = await axios.post("/auth/login", {
+      data: {
+        username,
+        password,
+      },
+    });
+    const data = res.data;
+
+    if (!data.sucess) {
+      return res.status;
+    }
+    return data.result;
+  }
+
+  async register(username, password, display_name) {
+    const res = await axios.post("/auth/register", {
+      data: {
+        username,
+        password,
+        display_name,
+      },
+    });
+
+    if (!data.sucess) {
+      if (data.message == "User already exists") return data.message;
+      return res.status;
+    }
+    return data.result;
+  }
 }
 
 export default new UserAPI();
